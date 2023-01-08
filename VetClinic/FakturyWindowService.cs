26a07223e3250fe5f5e83e41dd5165eb8132bf57
Zenber <perevertenser@ukr.net>
2023-01-08 16:16:44 +0100

    Combo box faktury update

--- VetClinic/FakturyWindowService.cs
+++ VetClinic/FakturyWindowService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using VetClinic.View;
 using VetClinic.Model;
+using VetClinic.ViewModel;
 
 namespace VetClinic
 {
@@ -22,7 +23,24 @@ namespace VetClinic
 
         public void AddFakturu() {
             // TODO FakturaWindow add logic
+            var window = ListOfWindows.GetFakturyWindow();
+
+            string jmenoMazlicka = new MainViewModel().SJmenoMazlicka.First().Jmeno;
+            string primeniMajitele = new MainViewModel().SJmenoMazlicka.First().PrimeniMajitele;
+            DateTime? dateTime = null;
+            string konecnaCena = window.konecnaCena.Text;
+            string oddeleni = new MainViewModel().SOddeleni;
+
+            if (window.datumVystaveniDatePicker.SelectedDate.HasValue)
+            {
+                dateTime = window.datumVystaveniDatePicker.SelectedDate.Value;
+            }
+
+            bool canAdd = jmenoMazlicka != null && primeniMajitele != null 
+                          && dateTime != null && konecnaCena != "" && oddeleni != null;
         }
+
+
 
         public void CloseWindow()
         {
